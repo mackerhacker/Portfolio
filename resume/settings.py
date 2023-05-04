2302db8d0d2c2a6c74fab86ad64ab97d885b5413
Hritik Jaiswal <97115479+mackerhacker@users.noreply.github.com>
2023-05-04 15:08:12 +0530

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4bb^-3i+!=6-35@$t@ku=a*mqw&5s5c2l#p!tcom6h5*r^m43v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hritikjaiswal.azurewebsites.net']
 
